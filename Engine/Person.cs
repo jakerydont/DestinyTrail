74f1a14668606392add6021adb0d89ac9632b197
Jakery <jakerydont@gmail.com>
2024-10-14 21:41:51 -0400

    override

--- Engine/Person.cs
+++ Engine/Person.cs
@@ -5,5 +5,10 @@ namespace DestinyTrail.Engine
         public required int ID { get; set; }  // Unique identifier for the person
         public required string Name { get; set; } // Name of the person
         public required Status Status { get; set; } // Status of the person (now strongly typed)
+
+        public override string ToString()
+        {
+            return $"Name ({ID})";
+        }
     }
 }
