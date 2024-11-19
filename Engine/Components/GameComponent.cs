e09dfa34bdfa67c30eb3debed3d1696101edcfda
jakerydont <jakerydont@gmail.com>
2024-11-19 11:16:49 -0500

    fixme note

--- Engine/Components/GameComponent.cs
+++ Engine/Components/GameComponent.cs
@@ -6,6 +6,7 @@ namespace DestinyTrail.Engine
 
         public override string ToString() => Name;
 
+        // FIXME: This method shouldn't be necessary. Tokenized strings think we're trying to call MemoryExtensions.ToLower() if we don't spell this out (even without override keyword). I don't know why.
         public string ToLower() => Name.ToLower();
 
         public static implicit operator string(GameComponent gc) => gc.Name;
