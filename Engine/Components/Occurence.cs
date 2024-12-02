26a88e7621c3d35d35e9244ed7178c0e6022258f
jakerydont <jakerydont@gmail.com>
2024-12-02 12:26:38 -0500

    parameterless constructor

--- Engine/Components/Occurence.cs
+++ Engine/Components/Occurence.cs
@@ -5,5 +5,13 @@ namespace DestinyTrail.Engine
         public required string DisplayText { get; set; }
         public double Probability { get; set; }
         public required string Effect {get;set;}
+        
+        public Occurrence(string name, string displayText, double probability, string effect)
+        {
+            Name = name;
+            DisplayText = displayText;
+            Probability = probability;
+            Effect = effect;
+        }
     }
 }
