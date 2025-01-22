b69f1cbda944c5a73fabe98321e1026bb80b26fb
Jakery <jakerydont@gmail.com>
2025-01-22 14:20:11 -0500

    deserialize debug

--- Engine/Abstractions/YamlDeserializer.cs
+++ Engine/Abstractions/YamlDeserializer.cs
@@ -1,15 +1,26 @@
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.Serialization;
 using YamlDotNet.Serialization;
 
 namespace DestinyTrail.Engine.Abstractions;
 public class YamlDeserializer : IYamlDeserializer
 {
-    public T Deserialize<T>(string input)
+public T Deserialize<T>(string input)
+{
+    var deserializer = new DeserializerBuilder()
+        .WithTypeResolver(new StaticTypeResolver())
+        .Build();
+    try
     {
-        var deserializer = new DeserializerBuilder()
-            .WithTypeResolver(new StaticTypeResolver())
-            .Build();
-        return deserializer.Deserialize<T>(input);
+        return deserializer.Deserialize<T>(input) 
+            ?? throw new Exception($"Deserialization failed: input: {input}, type: {typeof(T)}");
     }
+    catch (Exception ex)
+    {
+        throw new Exception($"Error deserializing input to type {typeof(T)}: {ex.Message}", ex);
+    }
+}
 
     public class StaticTypeResolver : ITypeResolver
     {
