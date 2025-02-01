e847ff959b79bfeb7c584649deedce87c0730a85
Jakery <jakerydont@gmail.com>
2025-02-01 12:42:15 -0500

    Fix blazor display and game loop

--- DestinyTrail.Blazor/Components/BlazorDisplay.razor.cs
+++ DestinyTrail.Blazor/Components/BlazorDisplay.razor.cs
@@ -1,27 +1,34 @@
 using System;
 using DestinyTrail.Engine;
+using Microsoft.AspNetCore.Components;
 
 namespace DestinyTrail.Blazor.Components;
 
 public partial class BlazorDisplay : IDisplay
 {
+    [Parameter]
+    public string Heading { get; set; } = "BlazorDisplay";
+
     public string outputText = "";
 
-    public List<string> Items { get;set; } = new();
+    public List<string> Items { get; set; } = new();
 
     public void Write(string message)
     {
-        outputText += message;
+        outputText += $"<div>{message}</div>";
+        InvokeAsync(StateHasChanged);
     }
 
     public void WriteTitle(string message)
     {
-        outputText += "TODO: BUILD A TITLE DISPLAY FOR THIS...<br/>" + message;
+        outputText += $"<div class='title'>{message}</div>";
+        InvokeAsync(StateHasChanged);
     }
 
     public void Clear()
     {
         outputText = "";
+        InvokeAsync(StateHasChanged);
     }
 
 
@@ -33,5 +40,6 @@ public partial class BlazorDisplay : IDisplay
     public void WriteError(string message)
     {
         outputText += $"<span class='error'>{message}</span>";
+        InvokeAsync(StateHasChanged);
     }
 }
