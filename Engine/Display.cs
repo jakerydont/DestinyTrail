55579f05a165c930f30b6f2d6f031dffa51d1415
Jakery <jakerydont@gmail.com>
2025-02-02 17:25:39 -0500

    suppress warning on empty method

--- Engine/Display.cs
+++ Engine/Display.cs
@@ -46,13 +46,15 @@ namespace DestinyTrail.Engine
                 }
             });
         }
-        
 
-         
+
+
+#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
         public virtual async Task ScrollToBottom()
         {
             // noop
         }
+#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
 
         public virtual async Task WriteError(string message)
         {
